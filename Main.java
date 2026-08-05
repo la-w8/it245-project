6f723facb4b4ceb8f7ac71f47fd02bedf797f75c
azizakf <azizakf@gmail.com>
2026-08-05 05:19:21 +0300

    Setting up Task cases

--- Main.java
+++ Main.java
@@ -10,7 +10,10 @@ public class Main {
 
         // [1] Setup Test Cases 
         System.out.println("\n[+] Initializing test tasks...");
-        //
+        
+        Task t1 = new Task(1, "Database Backup", 0, 3);
+        Task t2 = new Task(2, "Critical Security Patch", 1, 1);
+        Task t3 = new Task(3, "Generate Report", 2, 2);
         
         // [2] Test FIFO Queue Scheduling 
         System.out.println("\n[+] Testing FIFO Queue Scheduling ...");
